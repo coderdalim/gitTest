e28e4b9ba00433a1a1d4986b1cb367fcd4f0dd5a
dalim <dalim4871@gmail.com>
2018-12-19 11:28:01 +0600

    added a new method called greeting to hello.java

--- Hello.java
+++ Hello.java
@@ -2,4 +2,9 @@ public class Hello{
     public static void main(string[] args){
         System.out.println("vs code is Awesome....");
     }
+    greeting("Md dalim");
+}
+
+public static void greeting(String name){
+    System.out.println("Wlcomee " + name);
 }
